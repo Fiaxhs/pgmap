77ca3ce4b688341b8b19163d40f2d63eda22c6d7
Shahor <shahor@shahor.fr>
2016-08-04 01:17:57 +0200

    Control to search for a location
    Because it's not super easy when you're not in Paris anymore and it got
    super annoying to use because of that.
    I moved some stuff around but basically it's still the same as before,
    just with a new control and some refactoring for this purpose

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,76 @@
-var map,
+var Map,
     markers = {},
-    scannerMarker;
+    scannerMarker,
+    InitZoom = 16;
 
 function initMap() {
-    var initPosition = [48.869147, 2.3251892],
-        initZoom = 16;
+    var initPosition = [48.869147, 2.3251892];
+
     if (window.location.hash) {
         res = window.location.hash.match(/(\d+\.\d+)\/(\d+\.\d+)\/(\d+)/);
         if (res) {
             initPosition = [res[1], res[2]];
-            initZoom = res[3];
+            InitZoom = res[3];
         }
 
     }
-    map = L.map('map').setView(initPosition, initZoom);
+    Map = L.map('map').setView(initPosition, InitZoom);
+    addGeocoder();
+
     L.tileLayer(leafletURL, {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
-    }).addTo(map);
+    }).addTo(Map);
 
     scannerMarker = L.marker(initPosition, {
         title: 'Scanner',
-    }).addTo(map);
+    }).addTo(Map);
 
-    map.on('dragend', savePosition);
-    map.on('zoomend', savePosition);
-    map.on('click', function (e) {
-        fetch('/scan/' + e.latlng.lat + '/' + e.latlng.lng)
-        .then(function (response) {
-            response.json()
-            .then(function (json) {
-                console.log(json);
+    Map.on('dragend', savePosition);
+    Map.on('zoomend', savePosition);
+    Map.on('click', function (e) {
+        doScan(e.latlng.lat, e.latlng.lng)
+    });
+}
+
+function addGeocoder () {
+    L.Control.geocoder({
+        defaultMarkGeocode: false
+    }).on('markgeocode', function(e) {
+        var center = e.geocode.center
+
+        doScan(center.lat, center.lng)
+        .then(function () {
+            newLocation({
+                coords : {
+                    latitude : center.lat,
+                    longitude : center.lng
+                }
             })
-        });
+            savePosition(center)
+            Map.setView([center.lat, center.lng], InitZoom);
+        })
+    })
+    .addTo(Map);
+}
+
+function doScan(lat, lng) {
+    return fetch('/scan/' + lat + '/' + lng)
+    .then(function (response) {
+        return response.json()
+        .then(function (json) {
+            console.log(json);
+        })
     });
 }
 
 // Hash manipulation
-function savePosition() {
-    var center = map.getCenter();
-    window.location = '#' + center.lat + '/' + center.lng + '/' + map.getZoom();
+function savePosition(center) {
+    if (!center.lat && !center.lng) {
+        center = Map.getCenter();
+    }
+
+    window.location = '#' + center.lat + '/' + center.lng + '/' + Map.getZoom();
 }
 
 function getIcon(pokemonid) {
@@ -47,6 +78,10 @@ function getIcon(pokemonid) {
         iconUrl: ('images/icons/' + pokemonid + '.png'),
         iconSize: [48, 48]
     });
+}
+
+function newLocation (location) {
+    scannerMarker.setLatLng([location.coords.latitude, location.coords.longitude]);
 }
 
 // Add pokemon marker to map
@@ -68,10 +103,10 @@ function addPokemon(pokemon) {
             icon: icon,
             title: pokemonList[pokemon.pokemonid].name + ' (' + formattedTime + ')',
             opacity: pokemon.isLure ? 0.5 : 1
-        }).addTo(map);
+        }).addTo(Map);
         window.setTimeout(function() {
             delete markers[pokemon.id];
-            map.removeLayer(marker);
+            Map.removeLayer(marker);
         }, +pokemon.expiration - Date.now());
 
         markers[pokemon.id] = {marker: marker, pokemonid: pokemon.pokemonid, expiration: pokemon.expiration};
@@ -85,6 +120,4 @@ socket.on('newPokemon', function (pokemon){
     addPokemon(pokemon);
 });
 
-socket.on('newLocation', function (location){
-    scannerMarker.setLatLng([location.coords.latitude, location.coords.longitude]);
-});
+socket.on('newLocation', newLocation);
